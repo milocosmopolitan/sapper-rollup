4675cb474925cbc81bb7e60aadb322939f7b6a86
Minho Kang <minhokang@minhos-mbp-2.home>
2019-11-17 09:35:16 -0500

    feat: implement jest testing framework

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// jest.config.js
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html']
+};
